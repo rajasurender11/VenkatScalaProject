bce4a6e4cdd6e9414e13f60905203b484b4c81f0
rajasurender11 <rajasurender11@gmail.com>
2021-12-05 12:01:24 +0530

    scala collections

--- src/main/scala/com/venkat/demo/FileReaderMain.scala
+++ src/main/scala/com/venkat/demo/FileReaderMain.scala
@@ -1,5 +1,57 @@
 package com.venkat.demo
 
+import com.venkat.utils.DemoUtils
+
+import scala.io.Source
+
 object FileReaderMain {
+
+  def main(args: Array[String]): Unit = {
+
+    val fileLocMap = Map(
+      ("accounts_profile", "C:\\surender\\hadoop_course\\4_inputfiles\\accounts_profile.txt"),
+      ("atm_trans", "C:\\surender\\hadoop_course\\4_inputfiles\\atm_trans.txt"),
+      ("orders", "C:\\surender\\hadoop_course\\4_inputfiles\\orders.txt")
+    )
+
+    fileLocMap.foreach(elem => {
+      println("###################################")
+      println(s"processing for ${elem._1}")
+      val fileName = elem._1
+      val filePath = elem._2
+      val fileData = DemoUtils.readLocalFileAsList(filePath)
+
+
+      val resultList = fileName.toUpperCase() match {
+        case "ACCOUNTS_PROFILE" => processAccountsProfile(fileData)
+        case "ATM_TRANS" => processAtmTrans(fileData)
+        case "ORDERS" => processOrders(fileData)
+        case _ => List("EMPTY")
+      }
+
+      resultList.foreach(println)
+   })
+  }
+
+  def processAccountsProfile(data:List[String]):List[String] = {
+    data
+      .zipWithIndex
+      .map(rec => (rec._1,rec._2+1))
+      .filter(rec => rec._2%2 != 0)
+      .map(rec => rec._1)
+
+
+  }
+
+
+  def processAtmTrans(data:List[String]):List[String] = {
+
+    List("EMPTY")
+  }
+
+  def processOrders(data:List[String]):List[String] = {
+
+    List("EMPTY")
+  }
 
 }
--- src/main/scala/com/venkat/utils/DemoUtils.scala
+++ src/main/scala/com/venkat/utils/DemoUtils.scala
@@ -1,5 +1,11 @@
 package com.venkat.utils
 
+import scala.io.Source
+
 object DemoUtils {
+
+  def readLocalFileAsList(fileLoc: String): List[String] = {
+    Source.fromFile(fileLoc, "UTF-8").getLines().toList
+  }
 
 }
